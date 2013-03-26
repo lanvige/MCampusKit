3fe1107cbfcbf067bd38279d400513a21ea76793
Lanvige Jiang <lanvige@gmail.com>
2013-03-26 15:02:59 +0800

    init project

--- MCampusKit/MCKDefines.h
+++ MCampusKit/MCKDefines.h
@@ -1,13 +1,13 @@
 //
-//  MCampusKit.m
+//  MCKDefines.h
 //  MCampusKit
 //
 //  Created by Lanvige Jiang on 3/26/13.
 //  Copyright (c) 2013 Lanvige Jiang. All rights reserved.
 //
 
-#import "MCampusKit.h"
+#import <UIKit/UIKit.h>
 
-@implementation MCampusKit
+@interface MCKDefines : NSObject
 
 @end
---
+++ MCampusKit/MCKDefines.m
@@ -0,0 +1,13 @@
+//
+//  MCKDefines.m
+//  MCampusKit
+//
+//  Created by Lanvige Jiang on 3/26/13.
+//  Copyright (c) 2013 Lanvige Jiang. All rights reserved.
+//
+
+#import "MCKDefines.h"
+
+@implementation MCKDefines
+
+@end
